4926e96ef52c12c7bb8a9722bcf9bc1627a8f733
tsukasa-hash <71541755+tsukasa-hash@users.noreply.github.com>
2025-03-29 17:16:53 +0900

    typescriptを使えるようにした

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
     root: true,
     // extends: ['standard', 'plugin:vue/essential', 'plugin:prettier/recommended'],
-    extends: ['plugin:vue/essential', 'plugin:prettier/recommended'],
+    extends: ['plugin:vue/essential', 'plugin:prettier/recommended',
+      "eslint-config-prettier",],
     plugins: ['vue'],
+    parser: "@typescript-eslint/parser",
     rules: {
       'prettier/prettier': [
         'error',
@@ -16,5 +18,18 @@ module.exports = {
           endOfLine: 'auto'
         },
       ],
+      "@typescript-eslint/typedef": [
+      "error",
+      {
+        variableDeclaration: true,
+        memberVariableDeclaration: true,
+        propertyDeclaration: true,
+        variableDeclarationIgnoreFunction: true,
+        arrayDestructuring: true,
+        objectDestructuring: true,
+        parameter: true,
+        arrowParameter: true,
+      },
+    ],
     },
   }
