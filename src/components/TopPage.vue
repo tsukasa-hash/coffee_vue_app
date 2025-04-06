19c16cc894e973ed17bce60bbaf152cc010c4fd9
tsukasa-hash <71541755+tsukasa-hash@users.noreply.github.com>
2025-04-06 12:11:10 +0900

    vueのlint適用後

--- src/components/TopPage.vue
+++ src/components/TopPage.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <h1>何をする？</h1>
-    <router-link to="/">抽出する</router-link> <br />
-    <router-link to="/method_register">抽出を登録する</router-link> <br />
-    <router-link to="/method_list_viewer">抽出方法を見る</router-link>
-    <router-view></router-view>
+    <router-link to="/brewing">
+      抽出する
+    </router-link> <br>
+    <router-link to="/method_register">
+      抽出を登録する
+    </router-link> <br>
+    <router-link to="/method_list_viewer">
+      抽出方法を見る
+    </router-link>
+    <router-view />
   </div>
 </template>
 <script>
-export default { name: "topPage" };
+export default { name: "TopPage" };
 </script>
