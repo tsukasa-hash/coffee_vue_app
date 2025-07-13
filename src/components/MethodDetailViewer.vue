d8c0902025b4c63e78ba646422dbbdd472a32bf2
tsukasa-hash <71541755+tsukasa-hash@users.noreply.github.com>
2025-07-13 11:03:23 +0900

    経過時間に応じて手順の背景色を変更する

--- src/components/MethodDetailViewer.vue
+++ src/components/MethodDetailViewer.vue
@@ -97,51 +97,56 @@
           </button>
         </router-link>
       </div>
-      <table>
-        <thead>
-          <tr>
-            <th>詳細</th>
-            <th />
-            <th>合計</th>
-            <th />
-          </tr>
-        </thead>
-        <tbody
-          v-for="(step, index)
-            in
-              selectedMethod.procedure"
-          :key="index"
-        >
-          <tr>
-            <td style="text-align: left;rowspan:2">
-              {{ step.description }}
-            </td>
-            <td style="text-align: right">
-              {{ step.time }}秒
-            </td>
-            <td style="text-align: right">
-              {{ step.totalTime }}秒
-            </td>
-          </tr>
-          <tr>
-            <td />
-            <td style="text-align: right">
-              {{ step.amount }}ml
-            </td>
-            <td style="text-align: right">
-              {{ step.totalAmount }}ml
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>詳細</th>
+              <th />
+              <th>合計</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody
+            v-for="(step, index)
+              in
+                selectedMethod.procedure"
+            :key="index"
+          >
+            <tr>
+              <td :style="{ background: gradientStyle, textAlign: 'left', rowSpan: '2' }">
+                {{ step.description }}
+              </td>
+              <td style="text-align: right">
+                {{ step.time }}秒
+              </td>
+              <td style="text-align: right">
+                {{ step.totalTime }}秒
+              </td>
+            </tr>
+            <tr>
+              <td />
+              <td style="text-align: right">
+                {{ step.amount }}ml
+              </td>
+              <td style="text-align: right">
+                {{ step.totalAmount }}ml
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <p>
+        selectedMethod.procedure[0]:
+        {{ selectedMethod.procedure[0]?.time }}
+        initialTime:
+        {{ initialTime }}
+      </p>
+      <button @click="startProgress">
+        クリック
+      </button>
+      {{ elapsedTime }} / {{ totalTime }}
     </div>
-    <p>
-      <!-- {{ selectedMethod.procedure[0] }} -->
-      selectedMethod.procedure[0]:
-      {{ selectedMethod.procedure[0]?.time }}
-      initialTime:
-      {{ initialTime }}
-    </p>
   </div>
 </template>
 <script lang="ts">
@@ -159,26 +164,58 @@ export default defineComponent({
   data(): {
     initialTime: number,
     method: Method | undefined,
+    elapsedTime: number,
+    totalTime: number,
+    intervalId: number | null,
   } {
     return {
       initialTime: 0,
       method: new Method(),
+      elapsedTime: 0,
+      totalTime: 0,
+      intervalId: null as number | null,
     };
+  },
+  computed: {
+    // タイマーに応じて該当の手順だけ色を変える。
+    gradientStyle(): string {
+      const percent = Math.min(100, (this.elapsedTime / this.totalTime) * 100);
+      // 進捗に応じて色を変える
+      return `linear-gradient(to right,
+      #ff0000 0%, 
+      #ff0000 ${percent}%, 
+      #0000ff ${percent}%, 
+      #0000ff 100%)`;
+    },
   },
   watch: {
     selectedMethod() {
       this.method = this.selectedMethod ? this.selectedMethod : new Method();
       // FIXME:処理をきれいにしたい。initilaTimeの変数が必要なのか。
       // TODO:タイマーが終わったら次の手順に進みタイマーを実行する。
+      // TODO:タイマーのスタートと同時に、プログレスバーをスタートさせる。
       const rawProcedure = this.method.getProcedure();
       const procedure: Procedure[] = rawProcedure.map((p) => Object.assign(new Procedure("", 0, 0), p));
       const step1: Procedure = procedure[0];
       const time: number = Number(step1.getTime());
       this.initialTime = time;
+      this.totalTime = time;
     },
   },
   mounted() {
     this.method = this.selectedMethod;
+  },
+  methods: {
+    startProgress() {
+      this.elapsedTime = 0;
+      this.intervalId = setInterval(() => {
+        if (this.elapsedTime < this.totalTime) {
+          this.elapsedTime += 0.1;
+        } else {
+          clearInterval(this.intervalId!);
+        }
+      }, 100); // 0.1秒ごと
+    },
   },
 });
 </script>
